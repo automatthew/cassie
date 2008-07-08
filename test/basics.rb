3fbcd1f5c6b0b3b7f74df34000ca41209bd39663
Matthew King <automatthew@gmail.com>
2008-07-08 10:55:01 -0500

    broke tests into pieces

--- test/basics.rb
+++ test/basics.rb
@@ -4,72 +4,118 @@ require "#{here}/helper"
 describe "Casuistry" do
   
   before do    
-    @string = File.read("test.css")
-    @data = [
-        [ '.class_name1 .class_name2 .class_name3', 
-            [ 'font-size: 2em;', 'line-height: 3em;', 'border: 1px solid blue;']],
-        [ '#milk', 
-            ['background-color: blue;', 'border: 1px solid red']]
-    ]
-    @data2 = [
+
+    @fiddle = [
       ["ul", ["background: red;"]],
       ["ul li", ["color: green;"]],
-      ["ul li .ugly", ["color: aqua;"]],
-      ["ul .smurf .house", ["height: 256px;"]],
+      ["ul p.ugly", ["color: aqua;"]],
+      ["ul .smurf.house", ["height: 256px;"]],
       ["ul #asrael", ["padding: 10px;"]],
       [".gargamel", ["margin: 0px;"]],
       [".outer .middle .inner", ["top: 34px;"]]
     ]
     
-    @data3 = [
-      ["ul", ["background: red;", "width: 134px;"]], 
+    @xdata3 = [
+      ["ul", ["background: red;"]], 
       ["ul li .ugly", ["color: green;"]],
       ["ul .smurf .house", ["height: 256px;"]],
       ["ul #asrael", ["padding: 10px;"]],
       [".gargamel", ["margin: 0px;"]],
       [".outer .middle .inner", ["top: 34px;"]]
     ]
-    
-    @css1 = <<-CSS
-ul
-{
-  background: red;
-  width: 134px;
-}
-ul li .ugly
-{
-  color: green;
-}
-ul .smurf .house
-{
-  height: 256px;
-}
-ul #asrael
-{
-  padding: 10px;
-}
-.gargamel
-{
-  margin: 0px;
-}
-.outer .middle .inner
-{
-  top: 34px;
-}
-    CSS
+
   end
 
 
-
-  it "processes cssy code" do
-    c = Casuistry.new
-    c.instance_eval do
-      @background = "red"
+  it "can nest blocks" do
+    proc = lambda do
+      div do
+        form { width "35px" }
+        ul do
+          li { background :red }
+        end
+      end
     end
-    c.process(File.read( "#{here}/fiddle.cssy"))
-    c.data.should == @data2
-    # c.output.should == @css1
+    
+    data = [
+      ["div", []],
+      ["div form", ["width: 35px;"]],
+      ["div ul", []],
+      ["div ul li", ["background: red;"]]
+    ]
+    Cssy.process(&proc).should == data
   end
+  
+  it "can chain blocks" do
+    proc = lambda do
+      p.smurf { color :blue }
+      p.gnome.hat { color :red }
+    end
+    
+    data = [
+      ["p.smurf", [ "color: blue;"]],
+      ["p.gnome.hat", ["color: red;"]]
+    ]
+    Cssy.process(&proc).should == data
+  end
+  
+  it "can nest and chain" do
+    proc = lambda do
+      div do
+        span.ugly { font_family "Arial" }
+      end
+    end
+    
+    data = [
+      ["div", []],
+      ["div span.ugly", ["font-family: Arial;"]]
+    ]
+    Cssy.process(&proc).should == data
+  end
+
+  it "can chain and nest" do
+    proc = lambda do
+      ul.monkey do
+        li { list_style :none }
+      end
+    end
+    
+    data = [
+      ["ul.monkey", []],
+      ["ul.monkey li", ["list-style: none;"]]
+    ]
+    Cssy.process(&proc).should == data
+  end
+  
+  it "can chain and nest and chain and ..." do
+    proc = lambda do
+      div do
+        div.milk!.toast do
+          p.jam { margin :auto }
+          hr { background :green }
+        end
+        
+        p { border :none }
+      end
+    end
+    
+    data = [
+      ["div", []],
+      ["div div#milk.toast", []],
+      ["div div#milk.toast p.jam", ["margin: auto;"]],
+      ["div div#milk.toast hr", ["background: green;"]],
+      ["div p", ["border: none;"]]
+    ]
+    Cssy.process(&proc).should == data
+  end
+
+
+  # it "processes strings" do
+  #   c = Cssy.process(File.read( "#{here}/fiddle.cssy"))
+  #   c.should == @fiddle
+  #   # c.output.should == File.read( "#{here}/fiddle.css")
+  # end
+  
   
   
 end
