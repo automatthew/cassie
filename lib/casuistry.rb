1d660b7c651880b677fabfcff8dcf488f69c178e
Matthew King <automatthew@gmail.com>
2008-07-19 11:44:37 -0500

    comments and rearrange

--- lib/casuistry.rb
+++ lib/casuistry.rb
@@ -6,10 +6,6 @@ class Casuistry
     
   attr_reader :data
   
-  def self.process(*args,&block)
-    self.new.process(*args,&block)
-  end
-      
   def initialize(sel=nil)
     @data = []
     @selectors = [ sel ]
@@ -18,6 +14,10 @@ class Casuistry
     # Primitive state machine
     # possible states are :closed_block, :chain, :open_block
     @state = :closed_block
+  end
+  
+  def self.process(*args,&block)
+    self.new.process(*args,&block)
   end
   
   def process(*args, &block)
@@ -36,7 +36,74 @@ class Casuistry
     end.join
   end
 
-  # state transitions
+
+
+  # Pushes an empty array on the properties stack and registers
+  # that array (against the current selector) in @data
+  def new_property_set
+    @properties.push []
+    @data << [@selectors[-1], @properties[-1] ]
+  end
+
+
+  # Declare a CSS selector using a block.  May be chained and nested.
+  def selector(sel)
+    if block_given?
+      open_block(sel)
+      yield
+      closed_block
+    else
+      chain(sel)
+    end
+    self
+  end
+  
+  # Catch unknown methods and treat them as CSS class or id selectors.
+  def method_missing(name, &block)
+    sel = selectify(name)
+    if block_given?
+      open_block(sel)
+      yield
+      closed_block
+    else
+      chain(sel)
+    end
+    self
+  end
+  
+  # bang methods represent CSS ids.  Otherwise CSS classes.
+  def selectify(method_name)
+    matches = method_name.to_s.match( /([\w_]+)!$/)
+    matches ? "##{matches[1]}" : ".#{method_name}"
+  end
+
+
+  # define tag methods that delegate to selector
+  methods =  HTML_TAGS.map do |tag|
+    <<-METHOD
+    def #{tag}(&block)
+      selector('#{tag}', &block)
+    end
+    METHOD
+  end.join; module_eval(methods)
+
+  # define methods for CSS properties.
+  CSS_PROPERTIES.each do |method_name|
+    define_method method_name do |*args|
+      css_attr = method_name.gsub('_', '-')
+      property(css_attr, args)
+    end
+  end
+
+  # Add a property declaration string to the current selector's properties array.
+  def property(css_attr, *args)
+    @properties[-1] << "#{css_attr}: #{args.join(' ')};"
+  end
+  
+  ##  State transitions
+  
+  # Push the accumulated selector and a new property array onto the
+  # tops of their respected stacks
   def open_block(new_selector)
     case @state
     when :closed_block, :open_block
@@ -53,6 +120,7 @@ class Casuistry
     @state = :open_block
   end
 
+  # Pushes accumulated selector on the stack without generating a new properties array.
   def chain(new_selector)
     case @state
     when :closed_block, :open_block
@@ -67,6 +135,8 @@ class Casuistry
     @state = :chain
   end
 
+  # Pop the selector string and property array for the closing block
+  # off of their respective stacks
   def closed_block
     case @state
     when :open_block, :closed_block
@@ -80,75 +150,7 @@ class Casuistry
   end
 
 
-# normal methods
-
-  def selector(sel)
-    if block_given?
-      open_block(sel)
-      yield
-      closed_block
-    else
-      chain(sel)
-    end
-    self
-  end
-
-  def method_missing(name, &block)
-    sel = selectify(name)
-    if block_given?
-      open_block(sel)
-      yield
-      closed_block
-    else
-      chain(sel)
-    end
-    self
-  end
-
-  # def current_selector
-  #     @selectors[-1]
-  # end
-  # 
-  # def current_properties
-  #   @properties[-1]
-  # end
-
-  def new_property_set
-    @properties.push []
-    @data << [@selectors[-1], @properties[-1] ]
-  end
-
-  # define tag methods to delegate to selector
-  methods =  HTML_TAGS.map do |tag|
-    <<-METHOD
-    def #{tag}(&block)
-      selector('#{tag}', &block)
-    end
-    METHOD
-  end.join
-
-  module_eval methods
-
-  CSS_PROPERTIES.each do |method_name|
-    define_method method_name do |*args|
-      css_attr = method_name.gsub('_', '-')
-      property(css_attr, args)
-    end
-  end
-
-
-
-  def property(css_attr, *args)
-    @properties[-1] << "#{css_attr}: #{args.join(' ')};"
-  end
-
-
-
-
-  def selectify(method_name)
-    matches = method_name.to_s.match( /([\w_]+)!$/)
-    matches ? "##{matches[1]}" : ".#{method_name}"
-  end
+  
   
 end
 
